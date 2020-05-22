a00814211d62dacc8d6e3927aa3ec84f378c757a
Benjamin <benjaminbwm@gmail.com>
2020-05-23 00:40:56 +0100

    removed uneeded function

--- src/Matrix.hs
+++ src/Matrix.hs
@@ -22,43 +22,40 @@ showMatrix' (VCons vs vss) = showVector vs ++ ",\n " ++ showMatrix' vss
 showMatrix :: Show a => Matrix2 n m a -> String
 showMatrix vec = "[\n " ++ showMatrix' vec
 
-singleton :: a -> Matrix2 'One 'One a
-singleton = Vector.singleton . Vector.singleton
-
 transposeHelper :: Vector n a -> Matrix2 n m a -> Matrix2 n ('Succ m) a
 transposeHelper v m = binOpVec VCons v m
 
 transpose :: Matrix2 n m a -> Matrix2 m n a
-transpose (VSingle a) = fmap (Vector.singleton) a
+transpose (VSingle a) = fmap singleton a
 transpose (VCons v@(VCons _ _) vs) = binOpVec VCons v $ VCons topRow (transpose tails)
     where tails = fmap vecTail vs
           topRow = fmap vecHead vs
-transpose m@(VCons (VSingle _) _) = Vector.singleton $ fmap vecHead m
+transpose m@(VCons (VSingle _) _) = singleton $ fmap vecHead m
 
 appendRow :: Vector m a -> Matrix2 n m a -> Matrix2 (Add n 'One) m a
-appendRow v m = append m (Vector.singleton v)
+appendRow v m = append m (singleton v)
 prependRow :: Vector m a -> Matrix2 n m a -> Matrix2 (Add 'One n) m a
-prependRow v m = append (Vector.singleton v) m
+prependRow v m = append (singleton v) m
 
 appendCol :: Vector n a -> Matrix2 n m a -> Matrix2 n (Add m 'One) a
 appendCol v@(VSingle _) (VSingle vs) = VSingle (append vs v)
-appendCol (VCons v vs) (VCons m ms) = append (Vector.singleton (append m (Vector.singleton v))) (appendCol vs ms)
+appendCol (VCons v vs) (VCons m ms) = append (singleton (append m (singleton v))) (appendCol vs ms)
 prependCol :: Vector n a -> Matrix2 n m a -> Matrix2 n (Add 'One m) a
 prependCol v@(VSingle _) (VSingle vs) = VSingle (append v vs)
-prependCol (VCons v vs) (VCons m ms) = append (Vector.singleton (append (Vector.singleton v) m)) (prependCol vs ms)
+prependCol (VCons v vs) (VCons m ms) = append (singleton (append (singleton v) m)) (prependCol vs ms)
 
 squareMatrix2 :: a -> Matrix2 Two Two a
-squareMatrix2 a = VCons row $ Vector.singleton row
-    where row = VCons a $ Vector.singleton a
+squareMatrix2 a = VCons row $ singleton row
+    where row = VCons a $ singleton a
 squareMatrix3 :: a -> Matrix2 Three Three a
-squareMatrix3 a = VCons row $ VCons row $ Vector.singleton row
-    where row = VCons a $ VCons a $ Vector.singleton a
+squareMatrix3 a = VCons row $ VCons row $ singleton row
+    where row = VCons a $ VCons a $ singleton a
 
 binOpMat :: (a -> b -> c) -> Matrix2 n m a -> Matrix2 n m b -> Matrix2 n m c
 binOpMat f a b = binOpVec (binOpVec f) a b
 
 identity :: Num a => Vector n b -> Matrix2 n n a
-identity (VSingle _) = Matrix.singleton 1
+identity (VSingle _) = singleton $ singleton 1
 identity v@(VCons _ vs) = VCons (VCons 1 vs') ms
     where next = identity vs
           zeroed@(VCons _ zeroed') = fmap (\_ -> 0) v
@@ -69,11 +66,11 @@ multVects :: Num a => Vector m a -> Vector m a -> a
 multVects v1 v2 = sum $ binOpVec (*) v1 v2
 
 multVectMat :: Num a => Vector m a -> Matrix2 n m a -> Vector n a
-multVectMat xs (VSingle vs) = Vector.singleton $ multVects xs vs
+multVectMat xs (VSingle vs) = singleton $ multVects xs vs
 multVectMat xs (VCons v vs) = multVects xs v .:: multVectMat xs vs
 
 multiplyMat :: Num a => Matrix2 n m a -> Matrix2 m o a -> Matrix2 n o a
-multiplyMat (VSingle vs) b = Vector.singleton $ multVectMat vs $ transpose b
+multiplyMat (VSingle vs) b = singleton $ multVectMat vs $ transpose b
 multiplyMat (VCons v vs) b = multVectMat v transposed .:: multiplyMat vs b
     where transposed = transpose b
 
