db30fc71cfe55ea356b020b01aff65ee04051a13
Benjamin <benjaminbwm@gmail.com>
2021-03-09 00:40:56 +0000

    formatting changes, matrix construction changes, added tensorPower function and more comments

--- src/Quantum.hs
+++ src/Quantum.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
 module Quantum where
 
 import           Data.AdditiveGroup
@@ -9,13 +11,17 @@ import           Matrix
 import           Vector
 
 -- base type for qubits
-type CVVec n = VVec n (Complex Double)
+type CDouble = Complex Double
+type CVVec n = VVec n CDouble
 type Qubit = CVVec Two
-type TwoQubit = CVVec Four
+type NQubits n = CVVec (Exp Two n)
 
 -- for ease of use
 sqtwo :: Floating a => a
 sqtwo = 1 / sqrt 2
+
+i :: Num a => Complex a
+i = 0 C.:+ 1
 
 -- define some basic qubits/vectors, and qubit collections
 basisVec :: (Num a, KnownNat n) => Fin n -> VVec n a
@@ -42,51 +48,40 @@ minus = sqtwo *^ zero ^-^ sqtwo *^ one
 qubits :: [Qubit]
 qubits = [zero, one, plus, minus]
 
-makeTwoQubits :: [Qubit] -> [TwoQubit]
+makeTwoQubits :: [Qubit] -> [NQubits Two]
 makeTwoQubits qs = (.*.) <$> qs <*> qs
 
-twoQubits :: [TwoQubit]
+twoQubits :: [NQubits Two]
 twoQubits = makeTwoQubits [zero, one]
 
-consQubit :: Complex Double -> Complex Double -> Qubit
-consQubit a b = Mat $ (singleton a) :+ (singleton $ singleton b)
+consQubit :: CDouble -> CDouble -> Qubit
+consQubit a b = numMatFromList [[a], [b]]
 
 -- define some transformation matrices
 pauliX :: Num a => Matrix Two Two a
-pauliX = Mat $ (0 :+ singleton 1) :+ singleton (1 :+ singleton 0)
+pauliX = numMatFromList [[0, 1], [1, 0]]
 
 pauliY :: RealFloat a => Matrix Two Two (Complex a)
-pauliY =
-  Mat $ (0 :+ singleton (0 C.:+ (-1))) :+ singleton ((0 C.:+ 1) :+ singleton 0)
+pauliY = numMatFromList [[0, -i], [i, 0]]
 
 pauliZ :: Num a => Matrix Two Two a
-pauliZ = Mat $ (1 :+ singleton 0) :+ singleton (0 :+ singleton (-1))
+pauliZ = numMatFromList [[1, 0], [0, -1]]
 
 hadamard :: Floating a => Matrix Two Two a
-hadamard =
-  fmap (* sqtwo) $ Mat $ (1 :+ singleton 1) :+ singleton (1 :+ singleton (-1))
+hadamard = fmap (* sqtwo) $ numMatFromList [[1, 1], [1, -1]]
 
 -- a rotation matrix. produces a matrix which rotates by n radians
 rotation :: Floating a => a -> Matrix Two Two a
-rotation n = Mat $ (c :+ singleton (-s)) :+ singleton (s :+ singleton c)
+rotation n = numMatFromList [[c, -s], [s, c]]
  where
   c = cos n
   s = sin n
 
 cnot :: Num a => Matrix Four Four a
-cnot = generateMat cnotFunc
- where
-  cnotFunc m' n' =
-    fromIntegral $ fromEnum $ (m <= 2 && m == n) || (m > 2 && n > 2 && m /= n)
-    where (m, n) = (fromEnum m', fromEnum n')
+cnot = numMatFromList [[1, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]]
 
 cnot' :: Num a => Matrix Four Four a
-cnot' =
-  Mat
-    $  (1 :+ 0 :+ 0 :+ singleton 0)
-    :+ (0 :+ 0 :+ 0 :+ singleton 1)
-    :+ (0 :+ 0 :+ 1 :+ singleton 0)
-    :+ singleton ((0 :+ 1 :+ 0 :+ singleton 0))
+cnot' = numMatFromList [[1, 0, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0], [0, 1, 0, 0]]
 
 iden2 :: Num a => Matrix Two Two a
 iden2 = identity
@@ -101,33 +96,40 @@ swap =
 
 -- define the tensor product
 tensorProd
-  :: (Num a, KnownNat n, KnownNat m, KnownNat i, KnownNat j)
-  => Matrix n m a
-  -> Matrix i j a
-  -> Matrix (Mul n i) (Mul m j) a
-tensorProd n m = expandNested $ fmap (*^ m) n
+  :: (Num a) => Matrix n m a -> Matrix i j a -> Matrix (Mul n i) (Mul m j) a
+tensorProd n m = expandNested $ fmap (\a -> fmap (a *) m) n
 
 -- ease of use function for the tensor product
-(.*.)
-  :: (Num a, KnownNat n, KnownNat m, KnownNat i, KnownNat j)
-  => Matrix n m a
-  -> Matrix i j a
-  -> Matrix (Mul n i) (Mul m j) a
+(.*.) :: (Num a) => Matrix n m a -> Matrix i j a -> Matrix (Mul n i) (Mul m j) a
 (.*.) = tensorProd
 
 infixr 8 .*.
 
+-- apply the tensor product a given number of times.
+-- to use this function, use `tensorPower @(type number) matrix`
+-- for example, to tensor product matrix `a` gate three times, do `tensorPower @Three a`
+tensorPower
+  :: forall i a n m
+   . (Num a, KnownNat i)
+  => Matrix n m a
+  -> Matrix (Exp n i) (Exp m i) a
+tensorPower m = tensorPower' m (natSing :: NatS i)
+
+tensorPower' :: Num a => Matrix n m a -> NatS i -> Matrix (Exp n i) (Exp m i) a
+tensorPower' m (OneS   ) = m
+tensorPower' m (SuccS s) = m .*. (tensorPower' m s)
+
 -- transpose and get the conjugate of the given qubit
 -- effectively, find the bra
-conjTrans :: CVVec n -> HVec n (Complex Double)
+conjTrans :: CVVec n -> HVec n CDouble
 conjTrans v = fmap conjugate $ Matrix.transpose v
 
 -- using conjTrans, find the inner product of two qubits
-innerProduct :: CVVec n -> CVVec n -> Complex Double
+innerProduct :: CVVec n -> CVVec n -> CDouble
 innerProduct v v' = getVal $ conjTrans v *.* v'
 
 compBasis' :: (Num a, KnownNat n) => Vector n (VVec n a)
-compBasis' = generate basisVec
+compBasis' = generateVec basisVec
 
 -- define the computational basis
 compBasis :: Vector Two Qubit
@@ -146,10 +148,10 @@ measureIn v bs = fmap (\b -> (** 2) $ magnitude $ Quantum.innerProduct v b) bs
 measure :: Qubit -> Vector Two Double
 measure v = measureIn v compBasis
 
+-- given a list of vector-probability pairs, return Just the density matrix
+--  of all if the probabilities sum up to 1; else return Nothing
 getDensityMatrix
-  :: (KnownNat m)
-  => [(CVVec m, Complex Double)]
-  -> Maybe (Matrix m m (Complex Double))
+  :: (KnownNat m) => [(CVVec m, CDouble)] -> Maybe (Matrix m m CDouble)
 getDensityMatrix qs
   | sum (map snd qs) == 1 = Just
   $ foldr (^+^) zeroed [ p *^ (f tq) | (tq, p) <- qs ]
@@ -158,6 +160,9 @@ getDensityMatrix qs
   zeroed = generateMat (\_ _ -> 0)
   f m = m *.* conjTrans m
 
-calcProbability :: Matrix m m (Complex Double) -> CVVec m -> Complex Double
+-- given a probability matrix (as seen in previous function),
+--  and a complex number vector, return the probability of
+--  seeing that vector
+calcProbability :: Matrix m m CDouble -> CVVec m -> CDouble
 calcProbability densityMatrix base =
   getVal $ conjTrans base *.* densityMatrix *.* base
